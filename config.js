444da0ed407e94f99245dca3aaeb0af2fe2e9f0d
quochung <phanquochung673@gmail.com>
2026-09-24 10:16:34 +0700

    hihihihji

--- config.js
+++ config.js
@@ -5,15 +5,14 @@ window.LOVE_CONFIG = {
   petNameText: "MÓN QUÀ NHỎ NÈ",
   // Portrait-oriented photos are used as the large center image.
   heroPhotos: [
-    "assets/photos/01.jpeg",
-    "assets/photos/02.jpeg",
-    "assets/photos/03.jpeg",
-    "assets/photos/05.jpeg",
+    "assets/photos/1.jpeg",
+    "assets/photos/2.jpeg",
+    "assets/photos/3.jpeg",
+    "assets/photos/5.jpeg",
     "assets/photos/13.jpeg",
     "assets/photos/14.jpeg",
     "assets/photos/15.jpeg",
-    "assets/photos/17.jpeg",
-    "assets/photos/19.jpeg",
+    "assets/photos/4.jpeg",
   ],
   // All supplied images can appear as small floating memories.
   photos: Array.from(

34d3cd06338c332fccde91faa04a205966b84fb4
quochung <phanquochung673@gmail.com>
2026-09-24 10:46:05 +0700

    hihihlk

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 window.LOVE_CONFIG = {
   duration: 32.5,
+
   dateText: "TRUNG THU 2026",
   loveText: "GỬI NGỌC KHÁNH",
   petNameText: "MÓN QUÀ NHỎ NÈ",
-  // Portrait-oriented photos are used as the large center image.
+
+  // Ảnh lớn ở giữa
   heroPhotos: [
     "assets/photos/1.jpeg",
     "assets/photos/2.jpeg",
@@ -13,12 +15,34 @@ window.LOVE_CONFIG = {
     "assets/photos/14.jpeg",
     "assets/photos/15.jpeg",
     "assets/photos/4.jpeg",
+    "assets/photos/6.jpeg",
+    "assets/photos/7.jpeg",
+    "assets/photos/8.jpeg",
+    "assets/photos/3.jpeg",
+    "assets/photos/14.jpeg",
+    "assets/photos/13.jpeg",
+    "assets/photos/13.jpeg",
+    "assets/photos/6.jpeg",
+    "assets/photos/2.jpeg",
+    "assets/photos/2.jpeg",
+    "assets/photos/1.jpeg",
   ],
-  // All supplied images can appear as small floating memories.
-  photos: Array.from(
-    { length: 8 },
-    (_, i) => `assets/photos/${String(i + 1).padStart(2, "0")}.jpeg`,
-  ),
+
+  // Ảnh dùng cho đèn lồng / popup
+  photos: [
+    "assets/photos/1.jpeg",
+    "assets/photos/2.jpeg",
+    "assets/photos/3.jpeg",
+    "assets/photos/4.jpeg",
+    "assets/photos/5.jpeg",
+    "assets/photos/6.jpeg",
+    "assets/photos/7.jpeg",
+    "assets/photos/8.jpeg",
+    "assets/photos/13.jpeg",
+    "assets/photos/14.jpeg",
+    "assets/photos/15.jpeg",
+  ],
+
   particleLimitDesktop: 36000,
   particleLimitMobile: 16000,
   matrixDensity: 15,
